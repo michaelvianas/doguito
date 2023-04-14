dfa85810d9dfe40d0476ef59278170e8b9f27181
michaelvianas <vmichael116@gmail.com>
2023-04-14 15:26:52 -0300

    Validando CPF

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-import { validar } from './validacao.js'
+import { valida } from './validacao.js'
 
 const inputs = document.querySelectorAll('input')
 
 inputs.forEach(input => {
     input.addEventListener('blur', (evento) => {
-        validar(evento.target)
+        valida(evento.target)
     })
 })
