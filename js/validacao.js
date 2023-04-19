283915241fed42972a3f187ffda1174011369040
michaelvianas <vmichael116@gmail.com>
2023-04-19 00:28:32 -0300

    Aplicação de máscara

--- js/validacao.js
+++ js/validacao.js
@@ -54,6 +54,10 @@ const mensagensDeErro = {
     },
     estado: {
         valueMissing: 'O campo de estado não pode estar vazio.'
+    },
+    preco: {
+        valueMissing: 'O campo de preço não pode estar vazio.'
+
     }
 }
 
